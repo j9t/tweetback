e96bf2f2f779642f71b39f949d399e91a7eab725
Jens Oliver Meiert <jens@meiert.com>
2023-03-05 14:00:29 +0100

    Init
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "j9t", // No leading @ here
+	homeLabel: "meiert.com",
+	homeUrl: "https://meiert.com/en/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
